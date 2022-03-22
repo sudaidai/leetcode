7eec66d7543bd8cb0d53e4b7a54afde8259a3cf5
sudaidai <57120082+sudaidai@users.noreply.github.com>
2022-03-22 15:10:40 +0800

    1

--- src/com/company/Lc1249.java
+++ src/com/company/Lc1249.java
@@ -1,15 +1,17 @@
 package com.company;
 
+import java.util.Stack;
+
 public class Lc1249 implements Answer{
     @Override
     public int getIndex() {
         return 1249;
     }
 
-    public String minRemoveToMakeValid(String S) {
+    public String minRemoveToMakeValid_1(String s) {
         char tag = '_'; // use this tag to replace invalid parentheses
-        char[] ans = S.toCharArray();
-        int len = S.length(), stIx = 0, i = 0, j = 0;
+        char[] ans = s.toCharArray();
+        int len = s.length(), stIx = 0, i = 0, j = 0;
         int[] stack = new int[len+1];
         for (; i < len; i++) {
             if (ans[i] == ')') {
@@ -27,6 +29,8 @@ public class Lc1249 implements Answer{
         // now, if all the parentheses '(' are all matched, stIx is going to be 0
         // and stack will record one '(' pos at least
         stack[stIx] = -1;
+
+        // initialize the stIx, since we would like to use it to skip '('
         stIx = 0;
 
         for (i = 0 ; j < len; j++) {
@@ -34,5 +38,32 @@ public class Lc1249 implements Answer{
             else if (ans[j] != tag) ans[i++] = ans[j]; // skip all the tag we recognize invalid
         }
         return new String(ans, 0, i);
+    }
+
+    public String minRemoveToMakeValid_2(String s) {
+        Stack<Integer> stack = new Stack<>();
+
+        char tag = '_';
+        char ch[] = s.toCharArray();
+
+        for(int i = 0; i < ch.length; i++){
+            if(ch[i] == '(') stack.push(i);
+            else if(ch[i] == ')'){
+                if(stack.isEmpty()) ch[i] = tag;
+                else stack.pop();
+            }
+        }
+
+        while(!stack.isEmpty()){
+            ch[stack.pop()] = tag;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < ch.length; i++){
+            if(ch[i] != tag) sb.append(ch[i]);
+        }
+
+        return sb.toString();
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Lc1249 lc = new Lc1249();
-        String testStr = "lee(t(c)o)de)";
-        System.out.println(lc.minRemoveToMakeValid(testStr));
+        
     }
 }
