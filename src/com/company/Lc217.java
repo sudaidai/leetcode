bdc8a33a94a6cfe61968ff1c981b135528f57d9b
sudaidai <57120082+sudaidai@users.noreply.github.com>
2022-06-22 17:53:57 +0800

    Create Lc217.java

---
+++ src/com/company/Lc217.java
@@ -0,0 +1,43 @@
+package com.company;
+
+/*
+ * 217. Contains Duplicate
+ * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ */
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Lc217 implements Answer{
+
+    @Override
+    public int getIndex() {
+        return 217;
+    }
+
+    // n:  nums.length
+    // TC: Θ(n*log(n)) ~ O(n^2), steps: n*log(n)+n | n^2 + n
+    // SC: O(1) ~ O(log(n))
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i=0 ; i<nums.length-1 ; i++) {
+            if (nums[i] == nums[i+1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // n:  nums.length
+    // TC: O(n), steps: n
+    // SC: O(n)
+    public boolean containsDuplicate_2(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i=0 ; i<nums.length ; i++) {
+            if(!set.add(nums[i])) return true;
+        }
+        return false;
+    }
+
+}
