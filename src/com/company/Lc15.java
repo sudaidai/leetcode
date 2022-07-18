dda6438ae965cea051d8e8d438383be14d3d604f
sudaidai <57120082+sudaidai@users.noreply.github.com>
2022-07-18 16:50:56 +0800

    Update Lc15.java

--- src/com/company/Lc15.java
+++ src/com/company/Lc15.java
@@ -8,17 +8,40 @@ package com.company;
  */
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
-public class Lc15 implements Answer{
+public class Lc15 implements Answer {
 
     @Override
     public int getIndex() {
         return 15;
     }
 
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<Integer> triplet = new ArrayList<>();
-        return null;
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> triplets = new LinkedList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) break;
+            if (i == 0 || nums[i] != nums[i-1]) {
+                int p1 = i + 1, p2 = nums.length - 1;
+                while (p1 < p2) {
+                    if (nums[p1] + nums[p2] == -nums[i]) {
+                        List<Integer> triplet = new ArrayList<>();
+                        triplet.add(nums[i]);
+                        triplet.add(nums[p1]);
+                        triplet.add(nums[p2]);
+                        triplets.add(triplet);
+                        while (p1 < p2 && nums[p1] == nums[p1 + 1]) p1++;
+                        while (p1 < p2 && nums[p2] == nums[p2 - 1]) p2--;
+                        p1++;
+                        p2--;
+                    } else if (nums[p1] + nums[p2] < -nums[i]) p1++;
+                    else p2--;
+                }
+            }
+        }
+        return triplets;
     }
 }
