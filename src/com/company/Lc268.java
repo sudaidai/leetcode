27e35b78c446a766cea0e6749a8bdad2248a2ad9
sudaidai <57120082+sudaidai@users.noreply.github.com>
2022-07-23 15:28:42 +0800

    Create Lc268.java

---
+++ src/com/company/Lc268.java
@@ -0,0 +1,27 @@
+package com.company;
+
+/**
+ * 268. Missing Number
+ * Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+ */
+
+public class Lc268 implements Answer {
+
+    @Override
+    public int getIndex() {
+        return 268;
+    }
+
+    @Override
+    public String getRoute() {
+        return "missing-number";
+    }
+
+    public int missingNumber(int[] nums) {
+        int xor = 0, i;
+        for (i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+        return xor ^ i;
+    }
+}
