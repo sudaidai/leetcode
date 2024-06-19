0f1e6edcec1082359413e75eca716e666c820913
sudaidai <57120082+sudaidai@users.noreply.github.com>
2024-06-19 20:08:45 +0800

    Create Lc21.java

---
+++ src/com/company/Lc21.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import com.company.data.ListNode;
+
+/*
+ 21. Merge Two Sorted Lists
+ You are given the heads of two sorted linked lists list1 and list2.
+ Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+ Return the head of the merged linked list.
+*/
+public class Lc21 implements Answer {
+
+    @Override
+    public int getIndex() {
+        return 21;
+    }
+
+    @Override
+    public String getRoute() {
+        return "merge-two-sorted-lists";
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+}
